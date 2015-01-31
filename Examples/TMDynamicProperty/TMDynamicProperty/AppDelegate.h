264c33648f1f46e35f2338a1247e3e74c4ca8362
Zitao Xiong <caoer115@gmail.com>
2015-01-31 00:48:47 -0500

    setup

---
+++ Examples/TMDynamicProperty/TMDynamicProperty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TMDynamicProperty
+//
+//  Created by Zitao Xiong on 1/31/15.
+//  Copyright (c) 2015 Zitao Xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Examples/TMDynamicProperty/TMDynamicProperty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TMDynamicProperty
+//
+//  Created by Zitao Xiong on 1/31/15.
+//  Copyright (c) 2015 Zitao Xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
